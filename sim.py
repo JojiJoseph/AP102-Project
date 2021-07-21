e7361f662d50629b674b57c97972bfa4a852684f
joji <jojijoseph@iisc.ac.in>
2021-07-21 10:28:52 +0530

    Implemented loading maps

--- sim.py
+++ sim.py
@@ -1,19 +1,47 @@
-import streamlit as st
+import tkinter as tk
+from tkinter import PhotoImage
+from tkinter import ttk
+import toml
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
 
-print("Hello")
+config = toml.load("config.toml")
+maps = config["maps"]
 
-"""
-TODO
+print(config)
 
-[] Load arena
+root = tk.Tk()
 
-[] Find astar path
+assets = {}
 
-[] Apply DWA
-"""
+icon_file_name = "icon.png"
+icon = PhotoImage(file=icon_file_name)
 
-st.title("Choose a map")
+# FIXME: Why icon is not showing up on titlebar?
+root.iconphoto(False, icon)
+root.geometry("1280x960")
+root.title(" AP102 Project")
 
-option = st.selectbox("Select a map", [1, 2, 3])
+combo_map = ttk.Combobox(master=root)
 
-st.button("Simulate")
+combo_map["values"] = list(maps.keys())
+combo_map.set(combo_map["values"][0])
+combo_map.state(['readonly'])
+combo_map.pack()
+
+
+def simulate():
+    map_name = maps[combo_map.get()]
+    map_img = imageio.imread(map_name)
+    map_img = np.array(map_img)[:, :, 0]
+
+    plt.figure()
+    plt.imshow(map_img, cmap="Accent")
+    plt.title(map_name)
+    plt.show()
+
+
+button_sim = ttk.Button(root, text="Simulate", command=simulate)
+button_sim.pack()
+root.mainloop()
