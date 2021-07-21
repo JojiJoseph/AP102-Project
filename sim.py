14f201ec106341b13d08e24581cdc5803cd59b6f
joji <jojijoseph@iisc.ac.in>
2021-07-21 12:33:32 +0530

    Integrating DWA is in progress

--- sim.py
+++ sim.py
@@ -1,3 +1,4 @@
+from dwa import dwa
 import tkinter as tk
 from tkinter import PhotoImage
 from tkinter import ttk
@@ -7,7 +8,11 @@ import numpy as np
 import imageio
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-from astar import Astar
+from astar import Astar, generate_trajectory
+from utils import *
+from astar import corners_to_route, get_corners
+
+from astar import generate_trajectory as traj
 
 config = toml.load("config.toml")
 maps = config["maps"]
@@ -42,16 +47,25 @@ canvas.get_tk_widget().pack()
 def simulate():
     map_name = maps[combo_map.get()]
     map_img = imageio.imread(map_name)
-    map_img = np.array(map_img)[:, :, 0]
+    map_img = np.array(map_img)[:, :, 0]/255.
 
     astar_ = Astar(map_img)
 
     x, y = astar_.shortest_path((0, 0), (19, 19))
 
     # plt.sca(ax)
+    corners = get_corners(x, y)
+    route, init_pose = corners_to_route(corners)
+    x_, y_, t = generate_trajectory(route, init_pose, v_turn=0.01, dt=0.1)
 
+    ref_path = ref_path = np.hstack(
+        [np.array(axis).reshape(-1, 1) for axis in (x_, y_, t)])
+
+    dwa_path = dwa(map_img.T, ref_path, init_pose, grid_res=1)
     ax.imshow(map_img, cmap="Accent")
     ax.scatter(x, y)
+    plt.plot(x_, y_)
+    plt.plot(dwa_path[:,0],dwa_path[:,1])
     # plt.title(map_name)
     canvas.draw()
     ax.clear()
